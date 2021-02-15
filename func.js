14e8aca51c26287e8023cb85590412db713c7435
NutTecho <tingting321@windowslive.com>
2021-02-15 20:54:25 +0700

    insert loop calculation

--- func.js
+++ func.js
@@ -18,6 +18,7 @@ function demo1(a,b)
 // }
 
 let but1 = document.getElementById('but1');
+let but2 = document.getElementById('but2');
 let message = document.getElementById('message');
 let inp1 = document.getElementById('input1');
 let inp2 = document.getElementById('input2');
@@ -26,11 +27,24 @@ function showmessage()
 {
     let num1 = Number(inp1.value);
     let num2 = Number(inp2.value);
-    let out1 = num1 + num2;
-    message.innerHTML = out1;   
+    let out1 = '';
+    for(i = 1;i < num2;i++)
+    {
+        out1 += '<p>';
+        out1 +=  num1 + ' x ' + i + ' = ' + num1 * i;
+        out1 += '<p>';
+        message.innerHTML = out1;   
+    }
+ 
+}
+
+function clearall()
+{
+    message.innerHTML = '';
 }
 
 but1.addEventListener('click',showmessage);
+but2.addEventListener('click',clearall);
 // console.log(content);
 
 // for (let i =1;i < 10; i++)
