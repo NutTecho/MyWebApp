6230e2b8242df6a03d4b843f388b81b3388b8a66
NutTecho <tingting321@windowslive.com>
2021-03-15 23:30:20 +0700

    add write

--- NodeJS1/nodejstest.js
+++ NodeJS1/nodejstest.js
@@ -1,15 +1,28 @@
-// var response = require('D:/VSCODE/MyWebApp/NodeJS1/method.js');
-var fs = require('fs');
-var readme = fs.readFileSync('D:/VSCODE/MyWebApp/NodeJS1/demo.txt','utf-8');
-var getdb = readme.split(',')
-console.log(getdb[0]);
-// fs.readFile('demo.txt', function (err, data) {
-//   if (err) return console.error(err);
-//   console.log(data.toString());
-// });
-
-
+//===========get data from other file===========
+// var response = require('./method.js');
 // console.log(response.pi);
 // console.log(response.add(10,2));
 // console.log(response.update());
+//==========================================
 
+//======read ,write file================
+// var fs = require('fs');
+// var readme = fs.readFileSync('./demo.txt','utf-8');
+// var getdb = readme.split(',')
+// console.log(getdb);
+
+
+// fs.mkdir('project',function() {
+//     fs.writeFileSync('./project/hellojs.txt',readme)
+// });
+//======================================
+
+// var http = require('http');
+// http.createServer(function (request, response) {
+//   response.writeHead(200, {'Content-Type': 'text/plain'});
+//   response.end('Hello World123');
+// }).listen(8081);
+
+// console.log('Server running at http://127.0.0.1:8081/');
+
+function printHello
